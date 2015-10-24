3e46e6ed8027a349046288043fbe7a549e479454
Olion23 <rysmith143@gmail.com>
2015-10-24 17:23:11 -0400

    UGA Hacks Scene
    Scene stuff

---
+++ UGA Hacks 2015/Assets/Bike/SpinningTire.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningTire : MonoBehaviour {
+
+	public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (speed, 0, 0, Space.Self);
+	}
+}
---
+++ UGA Hacks 2015/Assets/Scripts/HandlebarRotate.cs
@@ -0,0 +1,13 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class HandlebarRotate : MonoBehaviour {
+
+	//public float speed = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 1, 0, Space.Self);
+	}
+}
+*/
